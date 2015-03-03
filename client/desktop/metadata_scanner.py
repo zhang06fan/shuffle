0de032528938b61d6223a809159486c95f0b8fc5
Jie Feng <fengjie.87@gmail.com>
2015-03-03 22:18:16 +0800

    Add cluster method

--- client/desktop/metadata_scanner.py
+++ client/desktop/metadata_scanner.py
@@ -1,35 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
+import shutil
 import sys
-import MySQLdb
 import eyed3
+
+# TODO: eyed3 can only resolve mp3 mutagen will be used in the future
+#suffix = [".mp3", ".flac", ".ape", ".wma"]
+suffix = [".mp3"]
 
 def insert_metadata(filename):
     # TODO: need file extention check
     audio_file = eyed3.load(filename)
-    print "========================================="
+    print "=" * 79
     print audio_file.tag.artist
     print audio_file.tag.album
     print audio_file.tag.title
     print audio_file.tag.track_num
-    print audio_file.info.time_secs / 60, "mins", audio_file.info.time_secs % 60, "secs"
+    print "%d:%d" %(audio_file.info.time_secs / 60, audio_file.info.time_secs % 60)
     print audio_file.info.size_bytes / 1024, "KB"
     print audio_file.path
-    print "========================================="
+    print "=" * 79
 
+def fetch_tag():
+    pass
 
-def traverse_dir(path):
-    """This method is used to travel directories and insert tags to database"""
+def cluster(filename, audio):
+    artist_dir = audio.tag.artist
+    album_dir = artist_dir + "/" + audio.tag.album
+    if not os.path.exists(album_dir):
+        os.makedirs(album_dir)
+    shutil.move(filename, album_dir)
+
+def scan_dir(path):
+    """This method is used to travel directories and cluster songs by tag"""
     for lists in os.listdir(path):
         tmp = os.path.join(path, lists)
         if os.path.isdir(tmp):
-            traverse_dir(tmp)
+            scan_dir(tmp)
         else:
-            insert_metadata(tmp)
+            if not cmp(os.path.splitext(tmp)[1], ".mp3"):
+                #print tmp, os.path.splitext(tmp)[1]
+                insert_metadata(tmp)
 
 if __name__ == "__main__":
-    homedir = os.environ.get("HOME")
-    traverse_dir(homedir + "/Music")
-    print "done"
+    #print os.environ.get("HOME")
+    scan_dir(os.environ.get("HOME") + "/Music")
 
